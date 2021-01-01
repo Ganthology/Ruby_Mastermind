590e4c6644ac06d506b86c4b48977279b98dfbcc
Boon Kit <gbk1386@gmail.com>
2021-01-01 09:27:43 +0000

    Refactor the computer codemaker to new file

---
+++ computer.rb
@@ -0,0 +1,10 @@
+module Computer
+  #color_option = %w(R B G Y C M)
+  color_option = %w(1 2 3 4 5 6)
+  COLOR_OPTION = color_option.product(color_option).flatten
+
+  def Computer.codeMaker
+    COLOR_OPTION.sample(4).join
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,40 +1,21 @@
-=begin
-MASTERMIND game
-
-6 color options, 4 places
-color_option = %w(RED BLUE GREEN YELLOW CYAN MAGENTA)
-color_option = color_option.product(color_option).flatten
-
-4 holes for giving hint, red white empty dots
-
-4 options
-1. Human code picker, Human code guesser
-2. Human code picker, Computer code guesser
-3. Computer code picker, Human code guesser
-4. Computer code picker, Computer code guesser1
-
-for the computer code picker, use #sample method to choose from color_option
-secret_code = color_option.sample(4)
-
-classes needed: code(define the code logic), display(display the results), mastermind(for a complete round of game), player class and computer class(containing different approach to the game)
-=end
-
-# first version : computer generate random code, player guess the code
+require './computer.rb'
+require 'rainbow'
 
 class Mastermind
+  include Computer
   @@rounds = 1
   attr_accessor :secret_code
-  color_option = %w(R B G Y C M)
-  COLOR_OPTION = color_option.product(color_option).flatten
 
   def initialize
     puts 'instructions'
     puts 'options to start the game / enter to start the game'
-    @secret_code = COLOR_OPTION.sample(4).join
+    @secret_code = Computer.codeMaker
     p @secret_code
+
     until(@guess_code == @secret_code || @@rounds > 12)
       puts 'Guess a combination (Format: RGBC)'
       @guess_code = gets.chomp.upcase
+      printCode
       puts "Current round: #{@@rounds}"
       @@rounds += 1
       if @guess_code == @secret_code
@@ -44,6 +25,29 @@ class Mastermind
       end
     end
 
+  end
+
+  def instructions
+    
+  end
+
+  def printCode
+    @guess_code.split.each do |code|
+      case code
+      when '1'
+      print "#{Rainbow(' 1 ').background(:red)}"
+      when '2'
+      print "#{Rainbow(' 2 ').background(:blue)}"
+      when '3'
+      print "#{Rainbow(' 3 ').background(:green)}"
+       when '4'
+      print "#{Rainbow(' 4 ').background(:yellow)}"
+      when '5'
+      print "#{Rainbow(' 5 ').background(:cyan)}"
+      when '6'
+      print "#{Rainbow(' 6 ').background(:magenta)}"
+      end
+    end
   end
 end
 
